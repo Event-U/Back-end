85cac717895bd83ea5b5cc7751e45e482e74e4e0
IrfDev <irving.dev@icloud.om>
2020-03-18 12:37:10 -0600

    Fixing typos into Need Models

--- src/models/need.js
+++ src/models/need.js
@@ -1,29 +1,29 @@
 const mongoose = require('mongoose')
 
-const needSchema = mongoose.Schema ({
-	description: {
-		minlength: 5,
-		type: String,
-		maxlength: 200,
-		requerid: true
-	},
-	service: {
-		type: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'service'
-		}
-	},
-	quotation: {
-		type: [{
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'quotation'
-		}]
-	},
-	isAwared: {
-		type: Boolean,
-		requerid: true,
-		default: false
-	}
+const needSchema = mongoose.Schema({
+    description: {
+        minlength: 5,
+        type: String,
+        maxlength: 200,
+        required: true
+    },
+    service: {
+        type: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'service',
+            required: true
+        }
+    },
+    quotation: {
+        type: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'quotation'
+        }]
+    },
+    isAwared: {
+        type: Boolean,
+        default: false
+    }
 })
 
 module.exports = mongoose.model('Need', needSchema)
--- src/routers/need.js
+++ src/routers/need.js
@@ -5,102 +5,102 @@ const router = express.Router()
 
 
 router.get('/', async(req, res) => {
-	try{
-		const allNeed = await Need.getAll()
-		res.json({
-			success: true,
-			message: 'All Need',
-			data: {
-				Need: allNeed
-			}
-		})
-	} catch (error) {
-		res.json({
-			succes: false,
-			message: 'Something went wrong',
-			error: error
-		})
-	}
+    try {
+        const allNeed = await Need.getAll()
+        res.json({
+            success: true,
+            message: 'All Need',
+            data: {
+                Need: allNeed
+            }
+        })
+    } catch (error) {
+        res.json({
+            succes: false,
+            message: 'Something went wrong',
+            error: error
+        })
+    }
 })
 
 router.get('/:id', async(req, res) => {
-	try {
-		const { id } = req.params
-		const NeedFound = await Need.getById(id)
-			res.json({
-				success: true,
-				message: 'Need Found',
-				data: {
-					Need: NeedFound
-				}
-			})
-	}	catch (error) {
-		res.json({
-			succes: false,
-			message: 'Something went wrong',
-			error: error.message
-		})
-	}
+    try {
+        const { id } = req.params
+        const NeedFound = await Need.getById(id)
+        res.json({
+            success: true,
+            message: 'Need Found',
+            data: {
+                Need: NeedFound
+            }
+        })
+    } catch (error) {
+        res.json({
+            succes: false,
+            message: 'Something went wrong',
+            error: error.message
+        })
+    }
 })
 
 router.post('/', async(req, res) => {
-	try {
-		const data = req.body
-		const createNeed = await Need.create(data)
-		res.json({
-			success: true,
-			message: 'Created Need',
-			Need: createNeed
-		})
-	} catch (error) {
-		res.json({
-			succes: false,
-			message: 'Something went wrong',
-			error: error
-		})
-	}
+    try {
+        const data = req.body
+        const createNeed = await Need.create(data)
+        res.json({
+            success: true,
+            message: 'Created Need',
+            Need: createNeed
+        })
+    } catch (error) {
+        res.json({
+            succes: false,
+            message: 'Something went wrong',
+            error: error
+        })
+    }
 })
 
-router.patch('/:id', async (req, res) => {
-	try{
-		const { id } = req.params
-		const body = req.body
-		const NeedUpdate = await Need.updateById(id, body)
+router.patch('/:id', async(req, res) => {
+    try {
+        const { id } = req.params
+        const body = req.body
+        const NeedUpdate = await Need.updateById(id, body)
 
-		res.json ({
-			success: true,
-			message: `Need ${id} update`,
-			data: {
-				Need: NeedUpdate
-			}
-		})
-	} catch (error) {
-		res.json({
-			success: false,
-			message: 'Something went wrong',
-			error: error.message
-		})
-	} 
+        res.json({
+            success: true,
+            message: `Need ${id} update`,
+            data: {
+                Need: NeedUpdate
+            }
+        })
+    } catch (error) {
+        res.json({
+            success: false,
+            message: 'Something went wrong',
+            error: error.message
+        })
+    }
 })
 
 router.delete('/:id', async(req, res) => {
-	try{
-		const { id } = req.params
-		const deleteNeed = await Need.deleteById(id)
-		res.json({
-			succes: true,
-			message: 'Deleted need',
-			data: {
-				Need: deleteNeed
-			}
-		})
-	} catch (error) {
-		res.json({
-			succes: false,
-			message: 'Something went wrong',
-			error: error.message
-		})
-	}
+    try {
+        const { id } = req.params
+        const deleteNeed = await Need.deleteById(id)
+        res.json({
+            succes: true,
+            message: 'Deleted need',
+            data: {
+                Need: deleteNeed
+            }
+        })
+    } catch (error) {
+        res.json({
+            succes: false,
+            message: 'Something went wrong',
+            error: error.message
+        })
+    }
 })
 
 
